759eb63543fa7ee6720953d1e6b7ab1d46ab0e95
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-04 17:13:29 +0300

    factory

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 var app = angular.module('app', []);
+
+app.controller('firstCtrl', function($scope, myFactory) {
+  $scope.myFactory = myFactory;
+});
+
+app.controller('secondCtrl', function($scope, myFactory) {
+  $scope.myFactory = myFactory;
+});
+
+app.factory('myFactory', function() {
+  return {
+    hello: 'hello world'
+  }
+});
