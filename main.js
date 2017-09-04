c91da09ee8c1afbea755842c01fc73aa8288fd42
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-04 18:32:28 +0300

    method

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 var app = angular.module('app', []);
+
+app.controller('firstCtrl', function($scope, myFactory) {
+  $scope.hello = 'Hello world';
+
+  $scope.myFactory = myFactory;
+
+  $scope.getBookmarks = function() {
+    return "My bookmarks";
+  }
+
+  $scope.setHello = function(text) {
+    $scope.hello = text;
+  }
+});
+
+app.factory('myFactory', function() {
+  return {
+    hello: function() {
+      return 'hello world'
+    }
+  }
+})
