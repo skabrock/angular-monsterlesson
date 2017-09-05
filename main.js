4f9ba02357a0b5ffb7553a636869910bcdc350b6
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-05 13:27:59 +0300

    using templates

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 var app = angular.module('app', []);
+
+app.directive('fooBar', function() {
+  var bookmarks = [
+    {
+      id: 1,
+      name: 'AngularJS',
+    }, {
+      id: 2,
+      name: 'EmberJS'
+    }, {
+      id: 3,
+      name: 'ReactJS'
+    }
+  ];
+
+  return {
+    template: `
+      <strong>Hello wolrd from {{name}}</strong>
+      <div ng-repeat='bookmark in bookmarks'>{{bookmark.name}}</div>
+    `,
+    link: function (scope, element, attrs) {
+      console.log('foo-bar');
+      scope.name = "Roman";
+      scope.bookmarks = bookmarks;
+    }
+  }
+})
