197d4f578f6b840522205d6abee6969bcab269da
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-05 11:56:58 +0300

    nested controllers

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 var app = angular.module('app', []);
+
+app.controller('myBooksCtrl', function ($scope) {
+  $scope.showBook = function () {
+    console.log('this.is.some.book')
+  }
+})
+
+app.controller('angularBookCtrl', function ($scope) {
+  $scope.showBook = function () {
+    console.log('this.is.angular.book')
+  }
+})
+
+app.controller('emberBookCtrl', function ($scope) {
+
+})
