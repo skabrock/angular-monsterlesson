357ef6681f5d14a9ed3661b751ca2ffa091a53d9
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-05 13:07:44 +0300

    directive restrictions variants

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 var app = angular.module('app', []);
+
+app.directive('fooBar', function () {
+  return {
+    restrict: 'EACM',  // EA
+    link: function() {
+      console.log('directive')
+    }
+  }
+})
