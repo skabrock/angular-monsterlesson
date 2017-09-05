b766842f9abba3dae2ebcf38e75082b7d9484c49
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-05 15:52:52 +0300

    template url

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 var app = angular.module('app', []);
+
+app.directive('fooBar', function () {
+  var bookmarks = [
+    {
+      id: 1,
+      name: 'EmberJS'
+    }, {
+      id: 2,
+      name: 'AngularJS'
+    }
+  ]
+  return {
+    restrict: 'E',
+    templateUrl: 'bookmarks.html',
+    link: function (scope, element, attrs) {
+      scope.bookmarks = bookmarks;
+    }
+  }
+})
