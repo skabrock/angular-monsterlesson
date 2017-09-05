627c808d3d223ee22c72dc01ce9348848da846d4
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-05 15:14:37 +0300

    transclude

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 var app = angular.module('app', []);
+
+app.controller('mainCtrl', function($scope) {
+  $scope.name = 'Bob';
+})
+
+app.directive('fooBar', function () {
+  return {
+    restrict: 'E',
+    transclude: true,
+    template: 'This is super directive / <ng-transclude></ng-transclude> / ',
+    link: function(scope, element, attrs, ctrl, transclude) {
+      transclude(scope, function( clone, scope) {
+        element.append(clone);
+      })
+    }
+  }
+})
