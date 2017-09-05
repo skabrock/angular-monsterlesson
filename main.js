7091f2b94a6f0f9595e61963a38540fa0c4acc75
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-05 12:53:19 +0300

    filters

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 var app = angular.module('app', []);
+
+app.controller('mainCtrl', function($scope) {
+  $scope.money1 = "1.22$";
+  $scope.money2 = "$2.55";
+  $scope.money3 = "4.33";
+})
+
+app.filter('moneyFilter', function() {
+  return function (str) {
+    var lastChar = str.slice(-1),
+        firstChar = str.slice(0, 1),
+        slicedPart;
+
+    console.log({str})
+
+    if (lastChar === "$") {
+      slicedPart = str.slice(0, str.length - 1);
+      return '$' + slicedPart;
+    } else if (firstChar === "$") {
+      return str;
+    } else {
+      return "$" + str;
+    }
+  }
+})
