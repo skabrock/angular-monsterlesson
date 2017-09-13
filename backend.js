f4146c634b74cf99bb6f512f6061b82fd7c93c1c
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-13 14:18:48 +0300

    http client/server

---
+++ backend.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(cors());
+
+var books = [
+  {
+    name: 'AngularJS'
+  },
+  {
+    name: 'EmberJS'
+  },
+  {
+    name: 'ReactJS'
+  }
+];
+
+app.get('/books', function (req, res) {
+  res.send(books);
+});
+
+app.post('/books', function (req, res) {
+  books.push(req.body);
+  res.send(200);
+});
+
+var server = app.listen(3001, function () {
+  console.log('backend started');
+});
--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 var app = angular.module('app', []);
+
+app.controller('mainCtrl', function($http, $scope) {
+  $http.get('http://localhost:3001/books')
+    .then(function(res) {
+      $scope.books = res.data;
+    })
+    .catch(function(res) {
+      console.log('error in books get', res);
+    });
+
+  $scope.addBook = function(book) {
+    $http.post('http://localhost:3001/books', book)
+    .then( function(res) {
+      console.log('book successfully saved to backend');
+      $scope.books.push(book);
+      $scope.book = null;
+    })
+    .catch( function(res) {
+      console.log('error in book post', res);
+    })
+  }
+});
