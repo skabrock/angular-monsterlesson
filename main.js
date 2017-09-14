14561e4041a15a211b73dfbb3c6dd0fbfd83eee4
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-14 11:06:49 +0300

    router

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'home.html'
+    })
+    .when('/posts', {
+      template: '<h1>Posts for my site</h1>'
+    })
+});
